0caf79383bd61eac7c903dc4aa25a26f839b8577
Ian <cryptoelmo22@gmail.com>
2022-05-28 18:41:21 +0800

    port over to polygon

--- backend/src/config.js
+++ backend/src/config.js
@@ -43,7 +43,7 @@ const extraMetadata = {
 // ** REQUIRED **
 const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
 const LIMIT = 2; // Your API key rate limit
-const CHAIN = 'rinkeby'; // only rinkeby or polygon
+const CHAIN = 'polygon'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
 const CONTRACT_NAME = 'Nothing But Flex';
@@ -52,7 +52,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0xe153aBff6bbfBBC24aD07F9A4714Dc963Df87842';
 const TREASURY_ADDRESS = '0xe153aBff6bbfBBC24aD07F9A4714Dc963Df87842';
 const MAX_SUPPLY = 500; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.0001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 1500; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
@@ -67,7 +67,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL.. NOPE.. Def needed ** 
-let CONTRACT_ADDRESS = "0xD640874D9511e0f22eB184f34ba4C6bFC5892105"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x5578F8E69fD0EBCdc1C7A9755970Ba7D92CaEA64"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
